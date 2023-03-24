0530945a681bc8255f90819021b66ca89c973c92
xenona <83364207+xenrelle@users.noreply.github.com>
2023-03-24 13:40:07 -0400

    Add files via upload

---
+++ NoSuperReactions.plugin.js
@@ -0,0 +1,57 @@
+/**
+ * @name NoSuperReactions
+ * @author xenona
+ * @description Removes super reactions.
+ * @version 0.9.9
+ */
+
+module.exports = class MyPlugin {
+	observer = null;
+	constructor(meta) {
+
+	}
+
+	start() {
+		BdApi.injectCSS("noSuperReactions",`
+			.my-button {
+				padding: 4px;
+				border-radius: 5px;
+				background: black;
+				color: white;
+			}
+
+			.hideEmoji-3W3sEI {
+				opacity: 1;
+			}
+
+			.effectsWrapper-1M6qbb,
+			div[aria-label="Add Super Reaction"],
+			.button-3bklZh[aria-label="Add Super Reaction. ( left)"],
+			div#message-actions-add-reaction-1,
+			div#message-add-reaction-1 {
+				display: none;
+			}
+		`);
+
+		this.observer = new MutationObserver(mutations => {
+			var reactions = document.querySelectorAll(".reaction-3vwAF2:not(.superReactionParsed)");
+			for (var reaction of reactions) {
+				if (!(!reaction.style.background)) { // if it has a background, it's a super reaction
+					reaction.style.display = "none"; // simply hide it instead of removing it, since removing it crashes discord
+				}
+				reaction.classList.add("superReactionParsed");
+			}
+		});
+
+		this.observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+	}
+
+	stop() {
+		BdApi.clearCSS("noSuperReactions");
+		this.observer.disconnect();
+		this.observer = null;
+	}
+};
