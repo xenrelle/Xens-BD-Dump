7e8ed177947315d0b3685a55f9f46f53243bea4e
xenona <83364207+vylryna@users.noreply.github.com>
2023-03-25 13:09:48 -0400

    remove super reactions from inside reaction window

--- plugins/NoSuperReactions/NoSuperReactions.plugin.js
+++ plugins/NoSuperReactions/NoSuperReactions.plugin.js
@@ -2,7 +2,7 @@
  * @name NoSuperReactions
  * @author xenona
  * @description Removes super reactions.
- * @version 1.0.1
+ * @version 1.0.2
  * @source https://github.com/xenrelle/Xens-BD-Dump/tree/main/plugins/NoSuperReactions
  * @updateUrl https://raw.githubusercontent.com/xenrelle/Xens-BD-Dump/main/plugins/NoSuperReactions/NoSuperReactions.plugin.js
  */
@@ -30,9 +30,16 @@ module.exports = class MyPlugin {
 
 		this.observer = new MutationObserver(mutations => {
 			var reactions = document.querySelectorAll(".reaction-3vwAF2:not(.superReactionParsed)");
+			var reactionsInMenu = document.querySelectorAll(".scroller-2GkvCq.thin-RnSY0a.scrollerBase-1Pkza4.fade-27X6bG > .reactionDefault-1Sjj1f:not(.superReactionParsed), .scroller-2GkvCq.thin-RnSY0a.scrollerBase-1Pkza4.fade-27X6bG > .reactionSelected-1aMb2K:not(.superReactionParsed)")
 			for (var reaction of reactions) {
 				if (!(!reaction.style.background)) { // if it has a background, it's a super reaction
 					reaction.style.display = "none"; // simply hide it instead of removing it, since removing it crashes discord
+				}
+				reaction.classList.add("superReactionParsed");
+			}
+			for (var reaction of reactionsInMenu) {
+				if (reaction.getAttribute("aria-label").includes("super reaction")) { // if it contains "super reaction" in it's aria label then, well, you get the idea.
+					reaction.style.display = "none";
 				}
 				reaction.classList.add("superReactionParsed");
 			}
