8e6564313e7e524d88795d0cc7124e4a4cc2b72d
xenona <83364207+vylryna@users.noreply.github.com>
2023-04-17 16:05:26 -0400

    waos

--- plugins/NoSuperReactions/NoSuperReactions.plugin.js
+++ plugins/NoSuperReactions/NoSuperReactions.plugin.js
@@ -1,9 +1,8 @@
 /**
  * @name NoSuperReactions
  * @author xenona
- * @authorId 621137770697457674
  * @description Removes super reactions.
- * @version 1.0.2
+ * @version 1.0.3
  * @source https://github.com/xenrelle/Xens-BD-Dump/tree/main/plugins/NoSuperReactions
  * @updateUrl https://raw.githubusercontent.com/xenrelle/Xens-BD-Dump/main/plugins/NoSuperReactions/NoSuperReactions.plugin.js
  */
@@ -20,41 +19,26 @@ module.exports = class MyPlugin {
 				opacity: 1;
 			}
 
+			/* Menu Options */
 			.effectsWrapper-1M6qbb,
 			div[aria-label="Add Super Reaction"],
 			.button-3bklZh[aria-label="Add Super Reaction. ( left)"],
 			div#message-actions-add-reaction-1,
-			div#message-add-reaction-1 {
+			div#message-add-reaction-1,
+			.navList-3uG4ub > div {
+				display: none;
+			}
+
+			/* Actual Super Reactions */
+			.reaction-3vwAF2[style*="background"],
+			.reactionDefault-1Sjj1f[aria-label*="super reaction"],
+			.reactionSelected-1aMb2K[aria-label*="super reaction"] {
 				display: none;
 			}
 		`);
-
-		this.observer = new MutationObserver(mutations => {
-			var reactions = document.querySelectorAll(".reaction-3vwAF2:not(.superReactionParsed)");
-			var reactionsInMenu = document.querySelectorAll(".scroller-2GkvCq.thin-RnSY0a.scrollerBase-1Pkza4.fade-27X6bG > .reactionDefault-1Sjj1f:not(.superReactionParsed), .scroller-2GkvCq.thin-RnSY0a.scrollerBase-1Pkza4.fade-27X6bG > .reactionSelected-1aMb2K:not(.superReactionParsed)")
-			for (var reaction of reactions) {
-				if (!(!reaction.style.background)) { // if it has a background, it's a super reaction
-					reaction.style.display = "none"; // simply hide it instead of removing it, since removing it crashes discord
-				}
-				reaction.classList.add("superReactionParsed");
-			}
-			for (var reaction of reactionsInMenu) {
-				if (reaction.getAttribute("aria-label").includes("super reaction")) { // if it contains "super reaction" in it's aria label then, well, you get the idea.
-					reaction.style.display = "none";
-				}
-				reaction.classList.add("superReactionParsed");
-			}
-		});
-
-		this.observer.observe(document.body, {
-			childList: true,
-			subtree: true
-		});
 	}
 
 	stop() {
 		BdApi.clearCSS("noSuperReactions");
-		this.observer.disconnect();
-		this.observer = null;
 	}
 };
--- themes/OldPing/README.md
+++ themes/OldPing/README.md
@@ -7,4 +7,4 @@ A BetterDiscord theme to revert the mention badge back to its old look.
 
 ## How to Install
 
-Download the `OldPing.theme.css` file and place it into your BetterDiscord thtmes folder. Afterwards, turn the theme on, and you should now see old pings.
+Download the `OldPing.theme.css` file and place it into your BetterDiscord themes folder. Afterwards, turn the theme on, and you should now see old pings.
